6352199148991349ee1f3f8aa12aa34b576a350a
MichelKansou <michel.kansou@outlook.fr>
2017-03-01 13:37:38 +0100

    PostController & Views :sparkles:

--- Sources/App/Controllers/PostController.swift
+++ Sources/App/Controllers/PostController.swift
@@ -3,27 +3,31 @@ import HTTP
 
 final class PostController: ResourceRepresentable {
     func index(request: Request) throws -> ResponseRepresentable {
-        return try Post.all().makeNode().converted(to: JSON.self)
+        let posts = try Post.all().makeNode().converted(to: JSON.self)
+        log.info("a nice information")
+        return try drop.view.make("Post/index", [
+            "posts": posts
+        ])
     }
 
-    func create(request: Request) throws -> ResponseRepresentable {
+    func store(request: Request) throws -> ResponseRepresentable {
+        log.info("post create")
         var post = try request.post()
         try post.save()
-        return post
+        log.info("post saved")
+        // guard let id = post.id!.int else { throw Abort.badRequest }
+        return Response(redirect: "/posts")
     }
 
     func show(request: Request, post: Post) throws -> ResponseRepresentable {
-        return post
+        return try drop.view.make("Post/show", [
+            "post": post
+        ])
     }
 
     func delete(request: Request, post: Post) throws -> ResponseRepresentable {
         try post.delete()
         return JSON([:])
-    }
-
-    func clear(request: Request) throws -> ResponseRepresentable {
-        try Post.query().delete()
-        return JSON([])
     }
 
     func update(request: Request, post: Post) throws -> ResponseRepresentable {
@@ -34,20 +38,13 @@ final class PostController: ResourceRepresentable {
         return post
     }
 
-    func replace(request: Request, post: Post) throws -> ResponseRepresentable {
-        try post.delete()
-        return try create(request: request)
-    }
-
     func makeResource() -> Resource<Post> {
         return Resource(
             index: index,
-            store: create,
+            store: store,
             show: show,
-            replace: replace,
             modify: update,
-            destroy: delete,
-            clear: clear
+            destroy: delete
         )
     }
 }
--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -4,21 +4,27 @@ import Foundation
 
 final class Post: Model {
     var id: Node?
+    var exists: Bool = false
+
+    var title: String
     var content: String
-    
-    init(content: String) {
-        self.id = UUID().uuidString.makeNode()
+
+    init(content: String, title: String) {
+        self.id = nil
+        self.title = title
         self.content = content
     }
 
     init(node: Node, in context: Context) throws {
         id = try node.extract("id")
+        title = try node.extract("title")
         content = try node.extract("content")
     }
 
     func makeNode(context: Context) throws -> Node {
         return try Node(node: [
             "id": id,
+            "title": title,
             "content": content
         ])
     }
@@ -30,16 +36,20 @@ extension Post {
         automatically for example. Remove on real models.
     */
     public convenience init?(from string: String) throws {
-        self.init(content: string)
+        self.init(content: string, title: string)
     }
 }
 
 extension Post: Preparation {
     static func prepare(_ database: Database) throws {
-        //
+        try database.create("posts", closure: { (post) in
+            post.id()
+            post.string("title")
+            post.string("content")
+        })
     }
 
     static func revert(_ database: Database) throws {
-        //
+        try database.delete("posts")
     }
 }
